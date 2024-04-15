05d113e5944c3e686812c77a63561e1ece57de8d
omkar-mohanty <franzohouser@gmail.com>
2024-04-15 19:51:07 +0530

    switch to rust

--- clrs/Algorithms/Common.hpp
+++
@@ -1,9 +0,0 @@
-#include<concepts>
-namespace Algorithms {
-	template<typename T>
-	requires std::integral<T>||std::floating_point<T>
-	constexpr bool compare(T & a, T & b) {
-		return a < b;
-	}
-
-}
